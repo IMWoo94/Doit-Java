2c3a289411e79fcb7cff2d3ba70dfbba7311ca99
IMWoo94 <leemeawoo94@gmail.com>
2023-10-23 12:30:22 +0900

    max value method create 2023.10.23

--- javaDoitAlgorithm/src/main/java/chap01/Max3m.java
+++ javaDoitAlgorithm/src/main/java/chap01/Max3m.java
@@ -11,4 +11,15 @@ public class Max3m {
 
 		return max;
 	}
+
+	public static void main(String[] args) {
+		System.out.println("max3(3,2,1) = " + max3(3, 2, 1));
+		System.out.println("max3(3,2,2) = " + max3(3, 2, 2));
+		System.out.println("max3(3,1,2) = " + max3(3, 1, 2));
+		System.out.println("max3(3,2,3) = " + max3(3, 2, 3));
+		System.out.println("max3(3,2,1) = " + max3(3, 2, 1));
+		System.out.println("max3(3,2,1) = " + max3(3, 2, 1));
+		System.out.println("max3(3,2,1) = " + max3(3, 2, 1));
+		System.out.println("max3(3,2,1) = " + max3(3, 2, 1));
+	}
 }
